550d64d6b3aaacc45b44e3cde3d5c19d598545d2
Drzola119 <zickowiko@gmail.com>
2026-04-05 08:21:14 +0100

    Add all 69 Algerian Wilayas

--- src/types/index.ts
+++ src/types/index.ts
@@ -70,14 +70,17 @@ export interface FilterState {
 
 export const WILAYAS = [
   'Adrar', 'Chlef', 'Laghouat', 'Oum El Bouaghi', 'Batna', 'Béjaïa', 'Biskra',
-  'Béchar', 'Blida', 'Bouira', 'Tamanrasset', 'Tébessa', 'Tlemcen', 'Tiaret',
+  'Béchar', 'Blida', 'Bouïra', 'Tamanrasset', 'Tébessa', 'Tlemcen', 'Tiaret',
   'Tizi Ouzou', 'Alger', 'Djelfa', 'Jijel', 'Sétif', 'Saïda', 'Skikda',
   'Sidi Bel Abbès', 'Annaba', 'Guelma', 'Constantine', 'Médéa', 'Mostaganem',
-  'Msila', 'Mascara', 'Ouargla', 'Oran', 'El Bayadh', 'Illizi', 'Bordj Bou Arreridj',
-  'Boumerdès', 'El Tarf', 'Tindouf', 'Tissemsilt', 'El Oued', 'Khenchela',
-  'Souk Ahras', 'Tipaza', 'Mila', 'Aïn Defla', 'Naâma', 'Aïn Témouchent',
-  'Ghardaïa', 'Relizane', 'Timimoun', 'Bordj Badji Mokhtar', 'Ouled Djellal',
-  'Béni Abbès', 'In Salah', 'In Guezzam', 'Touggourt', 'Djanet', "El M'ghair", 'El Meniaa'
+  "M'Sila", 'Mascara', 'Ouargla', 'Oran', 'El Bayadh', 'Illizi',
+  'Bordj Bou Arréridj', 'Boumerdès', 'El Tarf', 'Tindouf', 'Tissemsilt',
+  'El Oued', 'Khenchela', 'Souk Ahras', 'Tipaza', 'Mila', 'Aïn Defla', 'Naâma',
+  'Aïn Témouchent', 'Ghardaïa', 'Relizane', 'Timimoun', 'Bordj Badji Mokhtar',
+  'Ouled Djellal', 'Béni Abbès', 'In Salah', 'In Guezzam', 'Touggourt',
+  'Djanet', "El M'Ghair", 'El Meniaâ', 'Aflou', 'Bir El Ater', 'El Kantara',
+  'Barika', 'El Aricha', 'Ksar Chellala', 'Aïn Oussera', 'Messaad',
+  'Ksar El Boukhari', 'Bou Saâda', 'El Abiodh Sidi Cheikh'
 ] as const;
 
 export const CAR_BRANDS = [
